7dd803ddf77e7150283e81dc0e4e68582a7b4e0a
ashish.gupta01 <ashish.gupta01@BA-000175031.TI.ads>
2023-04-06 10:35:27 -0400

    First commit for email administration app

---
+++ EmailAdministrationApp/src/emailApp/EmailApplication.java
@@ -0,0 +1,12 @@
+package emailApp;
+
+public class EmailApplication {
+
+	public static void main(String[] args) {
+		Email eml = new Email("Ashish", "Gupta");
+		
+		System.out.println(eml.showInfo());
+		
+	}
+
+}
